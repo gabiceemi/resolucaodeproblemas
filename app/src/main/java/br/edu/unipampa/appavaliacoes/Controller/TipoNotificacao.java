cf9d717e6b6dfef44bb1ca5209647179ebf4c3d2
judson <judson.henrique01@gmail.com>
2018-11-20 23:45:14 -0200

    atualização interface tipo_notificação
    ultima atualização da tela de tipo de notificação

--- app/src/main/java/br/edu/unipampa/appavaliacoes/Controller/TipoNotificacao.java
+++ app/src/main/java/br/edu/unipampa/appavaliacoes/Controller/TipoNotificacao.java
@@ -16,6 +16,7 @@ public class TipoNotificacao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_notificacao);
+//        getSupportActionBar().setTitle("Tipo de Notificação");
 
         myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
 
