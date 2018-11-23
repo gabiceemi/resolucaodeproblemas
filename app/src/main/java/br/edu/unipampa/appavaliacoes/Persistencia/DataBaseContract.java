2a0310c2815317d704267759a5a9bd8b0c417230
Gabriela Córdova Medeiros <gabiceemi@gmail.com>
2018-11-23 17:47:38 -0200

    etc

--- app/src/main/java/br/edu/unipampa/appavaliacoes/Persistencia/DataBaseContract.java
+++ app/src/main/java/br/edu/unipampa/appavaliacoes/Persistencia/DataBaseContract.java
@@ -2,4 +2,29 @@ package br.edu.unipampa.appavaliacoes.Persistencia;
 
 public class DataBaseContract {
 
+	public static abstract class Avaliacao implements BaseColumns {
+        public static final String NOME_TABELA = "Avaliacao";
+        public static final String COLUNA_TITULO = "titulo";
+        public static final String COLUNA_DESCRICAO = "descricao";
+        public static final String COLUNA_DATA = "dia";
+        public static final String COLUNA_HORARIO = "horario";
+    }
+
+    public static abstract class TipoNotificacao implements BaseColumns {
+        public static final String NOME_TABELA = "TipoNotificacao";
+        public static final String COLUNA_TIPO = "tipo";
+        
+    }
+
+    public static abstract class Notificacao implements BaseColumns {
+        public static final String NOME_TABELA = "Notificacao";
+        public static final String COLUNA_TIPO = "tipo";
+        public static final String COLUNA_DATA_NOTIFICACAO = "datanotificacao";
+        public static final String COLUNA_HORARIO_NOTIFICACAO = "horarionotificacao";
+        public static final String COLUNA_MENSAGEM = "mensagem";
+        public static final String COLUNA_FK_AVALIACAO = "avaliacao";
+        public static final String COLUNA_FK_TIPO_NOTIFICACAO = "tiponotificacao";
+
+    }
+
 }
