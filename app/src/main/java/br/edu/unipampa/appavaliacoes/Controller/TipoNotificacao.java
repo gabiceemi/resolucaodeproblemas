7ccc43bcfc0d36f157329ddd96b9d78dd645c82b
judson <judson.henrique01@gmail.com>
2018-11-27 20:12:00 -0200

    Refatoramento tela tipo notificação

--- app/src/main/java/br/edu/unipampa/appavaliacoes/Controller/TipoNotificacao.java
+++ app/src/main/java/br/edu/unipampa/appavaliacoes/Controller/TipoNotificacao.java
@@ -1,31 +1,39 @@
 package br.edu.unipampa.appavaliacoes.Controller;
 
-import android.graphics.drawable.AnimationDrawable;
-import android.support.constraint.ConstraintLayout;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.edu.unipampa.appavaliacoes.R;
 
-public class TipoNotificacao extends AppCompatActivity {
-    ConstraintLayout myLayout;
-    AnimationDrawable animationDrawable;
 
+public class TipoNotificacao extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_notificacao);
-//        getSupportActionBar().setTitle("Tipo de Notificação");
 
-        myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
+        Button conf = (Button) findViewById(R.id.btn_confirmar);
+        conf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        animationDrawable = (AnimationDrawable) myLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(4000);
-        animationDrawable.setExitFadeDuration(4000);
-        animationDrawable.start();
+                Intent intent = new Intent(TipoNotificacao.this, AdicionarAvaliacaoActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        Button voltar = (Button) findViewById(R.id.btn_voltar);
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(TipoNotificacao.this, AdicionarAvaliacaoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
-
