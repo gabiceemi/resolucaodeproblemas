952eeef0160a1f52cb64b83022cc8cbe89d76845
Guilherme <guilthys@gmail.com>
2018-12-17 08:55:53 -0200

    Testando Thread em BackGroung -

--- app/src/main/java/br/edu/unipampa/appavaliacoes/Controller/MainActivity.java
+++ app/src/main/java/br/edu/unipampa/appavaliacoes/Controller/MainActivity.java
@@ -1,25 +1,37 @@
 package br.edu.unipampa.appavaliacoes.Controller;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import br.edu.unipampa.appavaliacoes.Persistencia.DataBasePersistencia;
 import br.edu.unipampa.appavaliacoes.Model.Avaliacao;
 import br.edu.unipampa.appavaliacoes.R;
+import br.edu.unipampa.appavaliacoes.Controller.NotificationChannel;
+import br.edu.unipampa.appavaliacoes.Controller.MainActivity;
+import br.edu.unipampa.appavaliacoes.Service.NotificationService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public FloatingActionButton adicionar;
     public ArrayList<Avaliacao> avaliacoes = new ArrayList<>();
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +39,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         adicionar = findViewById(R.id.adicionar);
         adicionar.setOnClickListener(this);
         apresentaAvaliacoes();
+        startService();
+
     }
+
+    private void startService() {
+
+        Intent serviceIntent = new Intent(this, NotificationService.class);
+            startService(serviceIntent);
+
+
+    }
+
+    public void stopService(){
+        Intent serviceIntent = new Intent(this, NotificationService.class);
+        stopService(serviceIntent);
+
+
+
+    }
+
+
+
+
+
+
+
 
     public ArrayList<Avaliacao> carregaAvaliacao() {
         DataBasePersistencia db = new DataBasePersistencia(this);
@@ -66,7 +103,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 bundle.putString("hora", avaliacoes.get(position).getHoraDaAvaliacao());
 
 
-
                 i.putExtras(bundle);
 
                 startActivity(i);
@@ -75,4 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
     }
+
+
+
 }
